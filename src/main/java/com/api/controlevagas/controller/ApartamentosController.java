c768b9ab9e1db6eeebae5fceb5e34209e5890f1c
alex.mariano.5000@gmail.com <alex.mariano.5000@gmail.com>
2023-03-26 16:53:38 -0300

    Ajustes

--- src/main/java/com/api/controlevagas/controller/ApartamentosController.java
+++ src/main/java/com/api/controlevagas/controller/ApartamentosController.java
@@ -4,28 +4,39 @@ import com.api.controlevagas.dto.ApartamentosDto;
 import com.api.controlevagas.entity.ApartamentosEntity;
 import com.api.controlevagas.repository.ApartamentosRepository;
 import com.api.controlevagas.service.ApartamentosService;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/vagas")
+@RequestMapping(value = "/vagas" ,produces = MediaType.APPLICATION_JSON_VALUE)
 public class ApartamentosController {
 
     @Autowired
     ApartamentosService apartamentosService;
+
     @Autowired
     ApartamentosRepository apartamentosRepository;
 
+    @ApiOperation(value = "Grava registro tabela Apartamentos")
     @PostMapping
     public ResponseEntity<Object> saveApartamento(@RequestBody ApartamentosDto apartamentosDto) {
         var apartamentosEntity = new ApartamentosEntity();
         BeanUtils.copyProperties(apartamentosDto, apartamentosEntity);
         return ResponseEntity.status(HttpStatus.CREATED).body(apartamentosService.save(apartamentosEntity));
+    }
+
+    @GetMapping(value = "todos")
+    public List<ApartamentosEntity> procura(){
+        var todosApartamentos = apartamentosRepository.findAll();
+        return todosApartamentos;
     }
 
     @GetMapping(value = "all")
